8db09083619b0ee254534f4a491ec7d8f35cf2b2
Hongchen Yang <hongchen_yang@outlook.com>
2025-12-02 00:01:52 +0100

    implement rms norm

--- assignment2/part2/gpt.py
+++ assignment2/part2/gpt.py
@@ -42,6 +42,8 @@ class RMSNorm(nn.Module):
     def forward(self, x):
         # Compute the norm of the input tensor and divide by the norm
         # Scale the normalized tensor by the learned weight parameter
+        rms = torch.sqrt(torch.mean(x ** 2, dim=-1, keepdim=True) + self.eps)
+        output = (x / rms) * self.weight
         return output
 
 class CausalSelfAttention(nn.Module):
